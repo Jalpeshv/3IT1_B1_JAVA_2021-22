aa37be45c82ea563075cd1d60d9045c6f5680b07
rency1192 <88083974+rency1192@users.noreply.github.com>
2021-08-04 05:42:34 -0700

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Reverse {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+
+            int a=sc.nextInt();
+            int rev=0;
+            while(a!=0)
+            {
+                int r=a%10;
+                rev=rev*10+r;
+                a=a/10;
+
+
+            }
+            System.out.println(rev);
+        }
+
+    }
+}
