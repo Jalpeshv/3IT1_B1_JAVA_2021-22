61d7c185dd293ee6f7cbd663c4b728ec39f9887f
vrushtikaria <88078729+vrushtikaria@users.noreply.github.com>
2021-08-05 15:21:41 +0530

    Add files via upload

---
+++ Small factorial.java
@@ -0,0 +1,21 @@
+//factorial by simple logic
+import java.util.*;
+public class Codechef {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();// for no of testcases
+
+        for(int i=0; i<n;i++)
+        {int fact = 1;
+            int num = sc.nextInt();
+            while (num != 0)
+            {
+                fact = fact * num;
+                num--;
+            }
+            System.out.println(fact);
+        }
+    }
+
+}
