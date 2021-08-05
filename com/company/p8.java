c33e784bc01c4d9aa5e7e718177d973d5d2a9615
Jay20it043 <20it043@charusat.edu.in>
2021-08-05 13:21:19 +0530

    Second Largest

---
+++ com/company/p8.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class p8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if(x>y){
+                if(x>z){
+                    if(y>z)
+                        a[i]=y;
+                    else
+                        a[i]=z;
+                }
+                else
+                {
+                    a[i]=x;
+                }
+            }
+            else{
+                if(x>z){
+                    a[i]=x;
+                }
+                else{
+                    if(y>z){
+                        a[i]=z;
+                    }
+                    else{
+                        a[i]=y;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
