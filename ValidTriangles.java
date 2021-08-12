7ec2ca434d8c6d456fe526393af64d9d85d214a4
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2021-08-12 12:51:36 +0530

    Add files via upload

---
+++ ValidTriangles.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ValidTriangles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if(x+y+z==180){
+                arr[i]=1;
+            }
+            else{
+                arr[i]=0;
+            }
+        }
+        for (int i=0;i<n;i++){
+            if(arr[i]==1){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
