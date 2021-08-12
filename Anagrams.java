ff9401cdcc0ead167246da73fdb003f68c8443e9
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2021-08-12 13:39:29 +0530

    Add files via upload

---
+++ Anagrams.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagram(String a, String b) {
+        char[] x=a.toLowerCase().toCharArray();
+        char[] y=b.toLowerCase().toCharArray();
+        java.util.Arrays.sort(x);
+        java.util.Arrays.sort(y);
+        return java.util.Arrays.equals(x,y);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
