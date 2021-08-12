d24d8e14c4b7514c4d04aa489677f2ac380960eb
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2021-08-12 12:52:37 +0530

    Add files via upload

---
+++ ChefandRemissness.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ChefandRemissness {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a>b)
+                System.out.print(a+" ");
+            else
+                System.out.print(b+" ");
+            System.out.println(a+b);
+        }
+    }
+}
