ab8e740099f4c3d1f52c567044bcc8a8914e61fb
krishnagevariya05 <88083861+krishnagevariya05@users.noreply.github.com>
2021-08-12 14:26:11 +0530

    Add files via upload

---
+++ Firstproject/src/com/company/Java_End_of_file.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Java_End_of_file {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int count=1;
+
+        while(sc.hasNext())
+        {
+            System.out.println(count + " " + sc.nextLine());
+            count++;
+        }
+    }
+}
