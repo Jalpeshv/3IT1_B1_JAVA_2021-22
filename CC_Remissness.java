7c61f3079fdc3cac713335707dadcae78a3bbaf5
HemilKevadiya <80502172+HemilKevadiya@users.noreply.github.com>
2021-08-12 13:36:50 +0530

    Add files via upload

---
+++ CC_Remissness.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class CC_Remissness {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int min[]=new int[t];
+        int max[]=new int[t];
+        for(int i=0;i<t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a>b)
+            {
+                min[i]=a;
+            }
+            else
+            {
+                min[i]=b;
+            }
+            max[i]=a+b;
+        }
+        for(int i=0;i<t;i++)
+        {
+            System.out.println(min[i]+" "+max[i]);
+        }
+    }
+}
