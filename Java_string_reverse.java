182852301e6dade35eeb56aadb5b69c9c86644e8
kapadiadev <80502782+kapadiadev@users.noreply.github.com>
2021-08-18 13:56:54 +0530

    Add files via upload

---
+++ Java_string_reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Java_string_reverse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String rev = "";
+        for(int i =A.length()-1; i>=0 ;i--)
+        {
+            rev += A.charAt(i);
+
+        }
+        if(rev.equals(A))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
