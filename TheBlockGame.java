86bb4e165335b3a7dabd9f22fc2058ad91905fd2
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2021-09-04 21:03:07 +0530

    Add files via upload

---
+++ TheBlockGame.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class TheBlockGame {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int temp = n;
+            int rev = 0;
+            while (temp > 0) {
+                int rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp = temp / 10;
+            }
+            if (rev == n) {
+                System.out.println("wins");
+            } else {
+                System.out.println("loses");
+            }
+        }
+    }
+}
