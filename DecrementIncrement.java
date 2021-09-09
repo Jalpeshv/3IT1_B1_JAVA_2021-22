ce6a3a6eb513797d3928ad7ba28eed868c4675b7
rency1192 <88083974+rency1192@users.noreply.github.com>
2021-09-08 22:28:33 -0700

    Add files via upload

---
+++ DecrementIncrement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class DecrementIncrement {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0)
+        {
+            System.out.println(++n);
+        }
+        else
+        {
+            System.out.println(--n);
+        }
+    }
+}
